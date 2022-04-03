7c89729acb16a8988f87d2f4ab9462e09f0fad0c
Tobias Lilsby <92530784+TPLilsby@users.noreply.github.com>
2022-04-03 10:35:28 +0200

    Add files via upload

---
+++ Planet.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planets
+{
+    internal class Planet
+    {
+        //Definere alle navnene på egenskaberne
+        public string name;
+        public double mass;
+        public double diameter;
+        public int density;
+        public double gravity;
+        public double rotation_period;
+        public double length_of_day;
+        public double distance_from_sun;
+        public double orbital_period;
+        public double orbital_velocity;
+        public int moon_temperature;
+        public int number_of_moons;
+        public bool ring_system;
+        
+        //Gør så man kan få og sætte værdierne til egenskaberne
+        public string Name { get; set; }
+        public double Mass { get; set; }
+        public double Diameter { get; set; }
+        public int Density { get; set; }
+        public double Gravity { get; set; }
+        public double Rotation_period { get; set; }
+        public double Length_of_day { get; set; }
+        public double Distance_from_sun { get; set; }
+        public double Orbital_period { get; set; }
+        public double Orbital_velocity { get; set; }
+        public int Moon_temperature { get; set; }
+        public int Number_of_moons { get; set; }
+        public bool Ring_system { get; set; }
+
+        //Føre egenskaberne ind i en Construtor så man kan bruge dem i programmet
+        public Planet(string Name, double Mass, double Diameter, 
+        int Density, double Gravity, double Rotation_period, 
+        double Length_of_day, double Distance_from_sun, double Orbital_period,
+        double Orbital_velocity, int Moon_temperature, int Number_of_moons, bool Ring_system)
+        {
+            name = Name;
+            mass = Mass;
+            diameter = Diameter;
+            density = Density;
+            gravity = Gravity;
+            rotation_period = Rotation_period;
+            length_of_day = Length_of_day;
+            distance_from_sun = Distance_from_sun;
+            orbital_period = Orbital_period;
+            orbital_velocity = Orbital_velocity;
+            moon_temperature = Moon_temperature;
+            number_of_moons = Number_of_moons;
+            ring_system = Ring_system;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿
+namespace Planets
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Indtastning af data fra planterne
+            Planet mercury = new Planet("Mercury", 0.330, 4879, 5427, 3.7, 3.7, 4222.6, 57.9, 88.0, 47.4, 167, 0, false);
+            Planet venus = new Planet("Venus", 4.87, 12.104, 5243, 8.9, -5832.5, 2802.0, 108.2, 224.7, 35.0, 464, 0, false);
+            Planet earth = new Planet("Earth", 5.97, 12.756, 5514, 9.8, 23.9, 24.0, 149.6, 365.2, 29.8, 15, 1, false);
+            Planet mars = new Planet("Mars", 0.642, 6792, 3933, 3.7, 24.6, 24.7, 227.9, 687.0, 24.1, -65, 2, false);
+            Planet jupiter = new Planet("Jupiter", 1898, 142.984, 1326, 23.1, 9.9, 9.9, 778.6, 4331, 13.1, -110, 67, true);
+            Planet saturn = new Planet("Saturn", 568, 120.536, 687, 9.0, 10.7, 10.7, 1433.5, 10.747, 9.7, -140, 62, true);
+            Planet uranus = new Planet("Uranus", 86.8, 51.118, 1271, 8.7, -17.2, 17.2, 2872.5, 30.589, 6.8, -195, 27, true);
+            Planet neptune = new Planet("Neptune", 102, 49.528, 1638, 11.0, 16.1, 16.1, 4495.1, 59.8, 5.4, -200, 14, true);
+            Planet pluto = new Planet("Pluto", 0.0146, 2370, 2095, 0.7, -153.3, 153.3, 5906.4, 90.56, 4.7, -225, 5, false);
+
+            //Liste med planeternes navne
+            List<Planet> planets = new List<Planet>();
+
+            planets.Add(mercury);
+            planets.Add(earth);
+            planets.Add(mars);
+            planets.Add(jupiter);
+            planets.Add(saturn);
+            planets.Add(uranus);
+            planets.Add(neptune);
+            planets.Add(pluto);
+
+            planets.Insert(1, venus);
+            planets.Remove(pluto);
+            planets.Insert(8, pluto);
+            Console.WriteLine(planets.Count());
+
+            string path = @"C:\prøve\text.txt";
+
+            //Udskriver planeternes navne
+            foreach (Planet planet in planets)
+            {
+                File.AppendAllText(path, planet.name + " " + planet.moon_temperature + "\n");
+                Console.WriteLine(planet.name);
+            }
+
+
+
+            //Liste over temparturen på planeterne
+            List<Planet> temperature = new List<Planet>();
+
+            //Tjekker om planeterne er under 0 grader
+            foreach (Planet planet in planets)
+            {
+                if (planet.moon_temperature < 0)
+                {
+                    temperature.Add(planet);
+                    
+                }
+            }
+            Console.WriteLine();
+            //Udskriver platerne med en temperatur på under 0 grader
+            foreach (Planet planet in temperature)
+            {
+                Console.WriteLine(planet.name);
+            }
+
+            //Liste over diameteren på planeterne
+            List<Planet> diameter = new List<Planet>();
+
+            //Tjekker hvilke planeter der har en diameter mellem 1000 - 5000
+            foreach (Planet planet in planets)
+            {
+                if (planet.diameter > 1000 && planet.diameter < 5000)
+                {
+                    diameter.Add(planet);
+
+                }
+            }
+            Console.WriteLine("....");
+            //Udskriver planterne med den rette diameter
+            foreach (Planet planet in diameter)
+            {
+                Console.WriteLine(planet.name);
+            }
+
+            //Sletter alle planterne i listen
+            planets.Clear();
+
+            Console.WriteLine("....");
+            //Udskriver planeternes navn igen
+            foreach (Planet planet in planets)
+            {
+                Console.WriteLine(planet.name);
+            }
+            
+        }
+    }
+}
